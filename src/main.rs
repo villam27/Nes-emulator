c5e68bf8b2b457d68e28556fdaeaf47775447533
villam27 <villam2703@protonmail.com>
2023-11-11 13:31:22 +0100

    [Add] Cpu struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+struct Cpu {
+    pc: u16,    //  Program Counter
+    sp: u8,     //  Stack Pointer
+    ac: u8,     //  Accumulator
+    rx: u8,     //  Register x
+    ry: u8,     //  Register y
+    status: u8, //  Processor Status
+
+    memory: [u8; 0xFFFF], //  https://www.nesdev.org/wiki/CPU_memory_map
+}
+
+impl Default for Cpu {
+    fn default() -> Cpu {
+        let memory: [u8; 0xFFFF] = [0; 0xFFFF];
+        Cpu {
+            pc: 0,
+            sp: 0xFF,
+            ac: 0,
+            rx: 0,
+            ry: 0,
+            status: 0,
+            memory,
+        }
+    }
+}
+
 fn main() {
+    let cpu = Cpu {
+        ..Default::default()
+    };
     println!("Nes emulator");
 }
